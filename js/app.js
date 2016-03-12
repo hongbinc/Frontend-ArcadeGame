4b6f4933663b09007f5289b4a149faa21d630fb3
hongbinc <hongbin1992@hotmail.com>
2016-03-12 14:32:09 -0500

    Add comments and correct code indentation

--- js/app.js
+++ js/app.js
@@ -1,67 +1,87 @@
-// Enemies our player must avoid
+/**
+ * @fileoverview Classes for the arcade game logic. 
+ * @author Hongbin Chen
+ */
+/**
+ * Enemy Class
+ * Constructs an enemy in the game.
+ */
 var Enemy = function (initX, initY, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Enemy's position and speed
     this.x = initX;
     this.y = initY;
     this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // Load enemy's image
     this.sprite = 'images/enemy-bug.png';
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/**
+ * Update the enemy's position
+ * @param dt A time delta between ticks
+ */
 Enemy.prototype.update = function (dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
     // Make the enemies move.
     this.x = this.x + this.speed * dt;
+    // If our enemies move off the screen, restart them at one block
+    // right before the start of the screen.
     if (this.x > 500) {
         this.x = 0;
+        // Randomnize enemy's speed every time enemy move off 
+        // the screen and start from the begining again.
         this.randamSpeed();
-    };
-    
+    }
+    // Check if there are collisions between the player and enemies.
     if (Math.abs(player.x - this.x) < 50 && Math.abs(player.y - this.y) < 50) {
-            player.reset();
-    };
-   
-
+        // If player is hit, reset player position.
+        player.reset();
+    }
 };
-
-// Draw the enemy on the screen, required method for game
+/**
+ * Renders and draws the enemy on the screen.
+ * @return {void}
+ */
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+/**
+ * Randomnize enemy's speed every time enemy move off 
+ * the screen and start from the begining again.
+ */
 Enemy.prototype.randamSpeed = function () {
     var speedRandom = Math.floor(Math.random() * maxSpeedEnemy + minSpeedEnemy);
     this.speed = speedRandom;
 };
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-
+/**
+ * Player class.
+ * Constructs a player.
+ * @constructor
+ */
 var Player = function (PlayerX, PlayerY) {
     this.x = PlayerX;
     this.y = PlayerY;
     this.sprite = 'images/char-horn-girl.png';
 };
-
-
-Player.prototype.update = function (dt) {
-    this.x*dt;
-    this.y*dt;
+/**
+ * Update player's position.
+ * @return {void}
+ */
+Player.prototype.update = function () {
+    // If player reach the water, reset player's position
     if (this.y < 0) {
         player.reset();
-    };
+    }
 };
+/**
+ * Renders and draws a player in the game.
+ * @return {void}
+ */
 Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+/**
+ * Handles keyboard pressed events.
+ * @param {string} key The id of pressed key
+ * @return {void}
+ */
 Player.prototype.handleInput = function (key) {
     switch (key) {
         case 'up':
@@ -82,32 +102,34 @@ Player.prototype.handleInput = function (key) {
             break;
         default:
             return;
-    };
-    
+    }
 };
 
-
+/**
+ * Reset player's position.
+ * @return {void}
+ */
 Player.prototype.reset = function () {
     this.x = 200;
     this.y = 400;
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var allEnemies = [],
     maxSpeedEnemy = 300,
     minSpeedEnemy = 110;
-
+// Initializes enimies and add them to the allEnemies[].
 for (var i = 0; i < 4; i++) {
+    // Randomnize enemy's speed
     var speed = Math.floor(Math.random() * maxSpeedEnemy + minSpeedEnemy);
+    // Initialize each enemy's position
     allEnemies.push(new Enemy(0, 65 + 80 * i, speed));
 }
-//console.log(speed);
-
+// Initialize a new player with x and y position.
 var player = new Player(200, 400);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/**
+ * Listens for key presses and sends the keys to your
+ * Player.handleInput() method.
+ */
 document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
